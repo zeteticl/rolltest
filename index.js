0db79e198941df91666b7fead7515e83bc94ad36
zeteticl <cougar.l@hotmail.com>
2016-12-13 17:56:42 +0800

    占

--- index.js
+++ index.js
@@ -133,6 +133,44 @@ CC後請輸入目標數字\
 	{        
 		return wod(trigger,mainMsg[1]);
 	}
+	
+	//擲杯功能
+	if (trigger.match(/^cup$|^擲杯$/) != null) {
+		return NormalDrawCup(trigger, mainMsg[1]);
+	}
+
+	//tarot 指令
+		if (trigger.match(/tarot|塔羅牌|塔羅/) != null) {
+
+		// if (inputStr.split(msgSplitor).length == 2 || inputStr.split(msgSplitor).length == 3) {
+
+			if (trigger.match(/每日|daily/)!= null) {
+				return NomalDrawTarot(mainMsg[1], mainMsg[2]);
+			}
+
+			if (trigger.match(/時間|time/)!= null) {
+				return MultiDrawTarot(mainMsg[1], mainMsg[2], 1);
+			}
+
+			if (trigger.match(/大十字|cross/)!= null) {
+				return MultiDrawTarot(mainMsg[1], mainMsg[2], 2);
+			}
+
+			// if (mainMsg[1] == '法典' || mainMsg[1] == '章典')
+			// 	return '你們打算懷著惡意傷害我的姊妹們，\
+			// 			\n因此我遵守我們名譽的章典， \
+			// 			\n給與你們殺死我的機會， \
+			// 			\n在此向你們申請決鬥！';
+
+			// if (mainMsg[1] == '抽牌' || mainMsg[1] == '決鬥' || mainMsg[1] == 'duel')
+			// 	return randomDuelReply(inputStr);
+
+			return MultiDrawTarot(mainMsg[1], mainMsg[2], 3); //預設抽 79 張
+		}
+
+	if (trigger == '猜拳') {
+			return RockPaperScissors(mainMsg[1], mainMsg[2]);
+		}
 
          //普通ROLL擲骰判定在此        
         if (inputStr.match(/\w/)!=null && inputStr.toLowerCase().match(/\d+d+\d/)!=null) {
@@ -142,6 +180,9 @@ CC後請輸入目標數字\
 }
 
 
+////////////////////////////////////////
+//////////////// 骰組開始
+////////////////////////////////////////      
 
 
                
@@ -371,6 +412,10 @@ function ArrMax (Arr){
   return finalStr;
 }
 
+function FunnyDice(diceSided) {
+	return Math.floor((Math.random() * diceSided)) //猜拳，從0開始
+}
+
 ////////////////////////////////////////
 //////////////// nechronica (NC)
 ////////////////////////////////////////
@@ -515,13 +560,364 @@ for (var i = 0; i < Number(match[1]); i++)
 ////////////////////////////////////////
 
         function randomReply() {
-          let rplyArr = ['你們死定了呃呃呃不要糾結這些……所以是在糾結哪些？', '在澳洲，每過一分鐘就有一隻鴨嘴獸被拔嘴。 \n我到底在共三小。', '嗚噁噁噁噁噁噁，不要隨便叫我。', '幹，你這學不會的豬！', '嘎嘎嘎。', 'wwwwwwwwwwwwwwwww', '為什麼你們每天都可以一直玩；玩就算了還玩我。', '好棒，整點了！咦？不是嗎？', '不要打擾我挖坑！', '好棒，誤點了！', '在南半球，一隻鴨嘴獸拍打他的鰭，他的嘴就會掉下來。 \n我到底在共三小。', '什麼東西你共三小。', '哈哈哈哈哈哈哈哈！', '一直叫，你4不4想拔嘴人家？', '一直叫，你想被淨灘嗎？', '幫主你也敢嘴？'];
+          let rplyArr = ['\
+你們死定了呃呃呃不要糾結這些……所以是在糾結哪些？', '\
+在澳洲，每過一分鐘就有一隻鴨嘴獸被拔嘴。 \n我到底在共三小。', '\
+嗚噁噁噁噁噁噁，不要隨便叫我。', '\
+幹，你這學不會的豬！', '\
+嘎嘎嘎。', '\
+wwwwwwwwwwwwwwwww', '\
+為什麼你們每天都可以一直玩；玩就算了還玩我。', '\
+好棒，整點了！咦？不是嗎？', '\
+不要打擾我挖坑！', '好棒，誤點了！', '\
+在南半球，一隻鴨嘴獸拍打他的鰭，他的嘴就會掉下來。 \n我到底在共三小。', '\
+什麼東西你共三小。', '\
+哈哈哈哈哈哈哈哈！', '\
+一直叫，你4不4想拔嘴人家？', '\
+一直叫，你想被淨灘嗎？', '\
+幫主你也敢嘴？', '\
+拔嘴的話，我的嘴巴會長出觸手，然後開花成四個花瓣哦 (´×`)', '\
+看看我！！我體內的怪物已經這麼大了！！', '\
+傳說中，凡是拔嘴過鴨嘴獸的人，有高機率在100年內死去。 \n我到底在共三小。', '\
+人類每花60秒拔嘴，就減少一分鐘的壽命。 \n我到底在共三小。', '\
+嘴被拔，就會掉。', '\
+你在大聲什麼啦！！！！', '\
+公道價，八萬一（伸手）。', '\
+你的嘴裡有異音（指）', '\
+幫主說，有人打你的左臉，你就要用肉食性猛擊咬斷他的小腿。'];
           return rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
         }
+		
        function randomLuck(TEXT) {
            let rplyArr = ['超吉','超級上吉','大吉','吉','中吉','小吉','吉','小吉','吉','吉','中吉','吉','中吉','吉','中吉','小吉','末吉','吉','中吉','小吉','末吉','中吉','小吉','小吉','吉','小吉','末吉','中吉','小吉','凶','小凶','沒凶','大凶','很凶'];
            return TEXT[0] + ' ： ' + rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
         }
+		
+	////////////////////////////////////////
+//////////////// Funny
+////////////////////////////////////////
+/* 猜拳功能 */
+function RockPaperScissors(HandToCal, text) {
+	let returnStr = '';
+	if (HandToCal == null) {
+		HandToCal = FunnyDice(3);
+	}
+	var hand = FunnyDice(3); // 0:石頭 1:布 2:剪刀
+
+	switch (hand) {
+		case 0: //石頭
+			returnStr = '我出石頭！\n';
+
+			if (HandToCal == '剪刀') returnStr += '哼哼你輸惹';
+			else if (HandToCal == '石頭') returnStr += '看來我們不相上下阿';
+			else if (HandToCal == '布') returnStr += '你好像有點強！';
+			else returnStr += '欸不對喔你亂出！';
+
+			break;
+
+		case 1: //布
+			returnStr = '我出布！\n';
+
+			if (HandToCal == '剪刀') returnStr += '讓你一次而已啦！';
+			else if (HandToCal == '布') returnStr += '原來平手...沒什麼嘛！';
+			else if (HandToCal == '石頭') returnStr += '哈哈你看看你！';
+			else returnStr += '別亂出阿會壞掉的';
+
+			break;
+
+		case 2: //剪刀
+			returnStr = '我出剪刀！\n';
+
+			if (HandToCal == '剪刀') returnStr += '平手 (  艸)';
+			else if (HandToCal == '布') returnStr += '贏了 (｀・ω・´)b';
+			else if (HandToCal == '石頭') returnStr += '輸惹 ゜。。゜(ノД‵)ノ・゜';
+			else returnStr += '亂出打你喔 (｀・ω・´)凸';
+
+			break;
+
+		default:
+			returnStr = '我出的是...欸不對你沒出喔！\n';
+			break;
+	}
+
+	return returnStr;
+}
+
+////////////////////////////////////////
+//////////////// Cup
+////////////////////////////////////////
+/* 擲杯功能 */
+function NormalDrawCup(chack, text) {
+	let returnStr = ''; //1為陽, 0為陰, 2定為中性
+
+	var result_left = [];
+	var result_right = [];
+	var cup_yes = 0,
+		cup_no = 0,
+		cup_laugh = 0,
+		cup_null = 0,
+		cup_else = 0;
+	// var max = 0;
+
+	for (var i = 0; i < 3; i++) //丟三次
+	{
+		result_left[i] = FunnyDice(3);
+		result_right[i] = FunnyDice(3);
+
+		if (result_left[i] == 0 && result_right[i] == 0) cup_no++;
+		if (result_left[i] == 0 && result_right[i] == 1) cup_yes++;
+		if (result_left[i] == 1 && result_right[i] == 0) cup_yes++;
+		if (result_left[i] == 1 && result_right[i] == 1) cup_laugh++;
+		if (result_left[i] == 2 && result_right[i] == 2) cup_null++;
+		// else cup_else ++;
+	}
+
+	if (text != null) {
+		if (cup_no >= 2) {
+			if (cup_no == 3)
+				returnStr += '三蓋杯' + ' ; ' + text;
+			else
+				returnStr += '蓋杯' + ' ; ' + text;
+		} else if (cup_yes >= 2) {
+			if (cup_yes == 3)
+				returnStr += '三聖杯' + ' ; ' + text;
+			else
+				returnStr += '聖杯' + ' ; ' + text;
+		} else if (cup_laugh >= 2) {
+			if (cup_laugh == 3)
+				returnStr += '三笑杯' + ' ; ' + text;
+			else
+				returnStr += '笑杯' + ' ; ' + text;
+		} else if (cup_null == 3) returnStr += '立杯' + ' ; ' + text;
+		else returnStr += '沒杯' + ' ; ' + text;
+	} else {
+		if (cup_no >= 2) {
+			if (cup_no == 3)
+				returnStr += '三蓋杯';
+			else
+				returnStr += '蓋杯';
+		} else if (cup_yes >= 2) {
+			if (cup_yes == 3)
+				returnStr += '三聖杯';
+			else
+				returnStr += '聖杯';
+		} else if (cup_laugh >= 2) {
+			if (cup_laugh == 3)
+				returnStr += '三笑杯';
+			else
+				returnStr += '笑杯';
+		} else if (cup_null == 3) returnStr += '立杯';
+		else returnStr += '沒杯';
+	}
+
+	return returnStr;
+}
+
+
+////////////////////////////////////////
+//////////////// Tarot
+////////////////////////////////////////
+function MultiDrawTarot(CardToCal, text, type) {
+	let returnStr = '';
+	var tmpcard = 0;
+	var cards = [];
+	var revs = [];
+	var i = 0;
+
+	if (type == 1) //時間之流
+	{
+		cards[0] = FunnyDice(79); //先抽第0張
+		revs[0] = FunnyDice(2);
+
+		for (i = 1; i < 3; i++) {
+			for (;;) {
+				tmpcard = FunnyDice(79);
+				if (cards.indexOf(tmpcard) === -1) //沒有重複，就這張了
+				{
+					cards.push(tmpcard);
+					revs[i] = FunnyDice(2);
+					break;
+				}
+			}
+		}
+
+		if (text != null)
+			returnStr += text + ': \n';
+
+		for (i = 0; i < 3; i++) {
+			if (i == 0) returnStr += '過去: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+			if (i == 1) returnStr += '現在: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+			if (i == 2) returnStr += '未來: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]);
+		}
+
+	} else if (type == 2) //塞爾特大十字
+	{
+		cards[0] = FunnyDice(79); //先抽第0張
+		revs[0] = FunnyDice(2);
+
+		for (i = 1; i < 10; i++) {
+			for (;;) {
+				tmpcard = FunnyDice(79);
+				if (cards.indexOf(tmpcard) === -1) //沒有重複，就這張了
+				{
+					cards.push(tmpcard);
+					revs[i] = FunnyDice(2);
+					break;
+				}
+			}
+		}
+
+		if (text != null)
+			returnStr += text + ': \n';
+
+		for (i = 0; i < 10; i++) {
+			if (i == 0) returnStr += '現況: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+			if (i == 1) {
+				if (revs[i] == 0) //正位
+					returnStr += '助力: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+				else
+					returnStr += '阻力: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+			}
+			if (i == 2) returnStr += '目標: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+			if (i == 3) returnStr += '基礎: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+			if (i == 4) returnStr += '過去: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+			if (i == 5) returnStr += '未來: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+			if (i == 6) returnStr += '自我: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+			if (i == 7) returnStr += '環境: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+			if (i == 8) {
+				if (revs[i] == 0) //正位
+					returnStr += '希望: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+				else
+					returnStr += '恐懼: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+			}
+			if (i == 9) returnStr += '結論: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]);
+
+		}
+
+	} else {
+
+		if (text == null)
+			returnStr = tarotCardReply(FunnyDice(79)) + ' ' + tarotRevReply(FunnyDice(2));
+		else
+			returnStr = tarotCardReply(FunnyDice(79)) + ' ' + tarotRevReply(FunnyDice(2)) + ' ; ' + text;
+	}
+
+
+	return returnStr;
+}
+
+function NomalDrawTarot(CardToCal, text) {
+	let returnStr = '';
+
+	if (text == null)
+		returnStr = tarotCardReply(FunnyDice(22)) + ' ' + tarotRevReply(FunnyDice(2));
+	else
+		returnStr = tarotCardReply(FunnyDice(22)) + ' ' + tarotRevReply(FunnyDice(2)) + ' ; ' + text;
+
+	return returnStr;
+}
+
+function tarotRevReply(count) {
+	let returnStr = '';
+
+	if (count == 0) returnStr = '＋';
+	if (count == 1) returnStr = '－';
+
+	return returnStr;
+}
+
+function tarotCardReply(count) {
+	let returnStr = '';
+
+	// returnStr = count + '愚者';
+	if (count == 0) returnStr = '愚者';
+	if (count == 1) returnStr = '魔術師';
+	if (count == 2) returnStr = '女祭司';
+	if (count == 3) returnStr = '女皇';
+	if (count == 4) returnStr = '皇帝';
+	if (count == 5) returnStr = '教皇';
+	if (count == 6) returnStr = '戀人';
+	if (count == 7) returnStr = '戰車';
+	if (count == 8) returnStr = '力量';
+	if (count == 9) returnStr = '隱者';
+	if (count == 10) returnStr = '命運之輪';
+	if (count == 11) returnStr = '正義';
+	if (count == 12) returnStr = '吊人';
+	if (count == 13) returnStr = '死神';
+	if (count == 14) returnStr = '節制';
+	if (count == 15) returnStr = '惡魔';
+	if (count == 16) returnStr = '高塔';
+	if (count == 17) returnStr = '星星';
+	if (count == 18) returnStr = '月亮';
+	if (count == 19) returnStr = '太陽';
+	if (count == 20) returnStr = '審判';
+	if (count == 21) returnStr = '世界';
+
+	if (count == 22) returnStr = '權杖一';
+	if (count == 23) returnStr = '權杖二';
+	if (count == 24) returnStr = '權杖三';
+	if (count == 25) returnStr = '權杖四';
+	if (count == 26) returnStr = '權杖五';
+	if (count == 27) returnStr = '權杖六';
+	if (count == 28) returnStr = '權杖七';
+	if (count == 29) returnStr = '權杖八';
+	if (count == 30) returnStr = '權杖九';
+	if (count == 31) returnStr = '權杖十';
+	if (count == 32) returnStr = '權杖侍者';
+	if (count == 33) returnStr = '權杖騎士';
+	if (count == 34) returnStr = '權杖皇后';
+	if (count == 35) returnStr = '權杖國王';
+
+	if (count == 36) returnStr = '聖杯一';
+	if (count == 37) returnStr = '聖杯二';
+	if (count == 38) returnStr = '聖杯三';
+	if (count == 39) returnStr = '聖杯四';
+	if (count == 40) returnStr = '聖杯五';
+	if (count == 41) returnStr = '聖杯六';
+	if (count == 42) returnStr = '聖杯七';
+	if (count == 43) returnStr = '聖杯八';
+	if (count == 44) returnStr = '聖杯九';
+	if (count == 45) returnStr = '聖杯十';
+	if (count == 46) returnStr = '聖杯侍者';
+	if (count == 47) returnStr = '聖杯騎士';
+	if (count == 48) returnStr = '聖杯皇后';
+	if (count == 49) returnStr = '聖杯國王';
+
+	if (count == 50) returnStr = '寶劍一';
+	if (count == 51) returnStr = '寶劍二';
+	if (count == 52) returnStr = '寶劍三';
+	if (count == 53) returnStr = '寶劍四';
+	if (count == 54) returnStr = '寶劍五';
+	if (count == 55) returnStr = '寶劍六';
+	if (count == 56) returnStr = '寶劍七';
+	if (count == 57) returnStr = '寶劍八';
+	if (count == 58) returnStr = '寶劍九';
+	if (count == 59) returnStr = '寶劍十';
+	if (count == 60) returnStr = '寶劍侍者';
+	if (count == 61) returnStr = '寶劍騎士';
+	if (count == 62) returnStr = '寶劍皇后';
+	if (count == 63) returnStr = '寶劍國王';
+
+	if (count == 64) returnStr = '錢幣一';
+	if (count == 65) returnStr = '錢幣二';
+	if (count == 66) returnStr = '錢幣三';
+	if (count == 67) returnStr = '錢幣四';
+	if (count == 68) returnStr = '錢幣五';
+	if (count == 69) returnStr = '錢幣六';
+	if (count == 70) returnStr = '錢幣七';
+	if (count == 71) returnStr = '錢幣八';
+	if (count == 72) returnStr = '錢幣九';
+	if (count == 73) returnStr = '錢幣十';
+	if (count == 74) returnStr = '錢幣侍者';
+	if (count == 75) returnStr = '錢幣騎士';
+	if (count == 76) returnStr = '錢幣皇后';
+	if (count == 77) returnStr = '錢幣國王';
+
+	if (count == 78) returnStr = '空白牌';
+
+	return returnStr;
+
+}
 		function Help() {
 			return randomReply() + '\n' + '\
 【擲骰BOT】v1.0 \
