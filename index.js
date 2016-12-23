896b53c1fb201f12de4ccee37721081f785e22c3
zeteticl <cougar.l@hotmail.com>
2016-12-24 01:13:59 +0800

    debug

--- index.js
+++ index.js
@@ -328,7 +328,7 @@ function ArrMax (Arr){
     let aaa = equation;
 	aaa = aaa.replace(/\d+[[]/, '(' );
 	aaa = aaa.replace(']', ')' ); 
-	let answer = aaa.replace(/[(]\d+|[)]/ig,"").toString();
+	let answer = eval(aaa.replace(/[(]\d+|[)]/ig,"").toString());
     finalStr = finalStr + i + '# ' + equation + ' = ' + answer + '\n';
     }
         
@@ -356,7 +356,7 @@ function ArrMax (Arr){
 	let aaa = equation;
 	aaa = aaa.replace(/\d+[[]/, '(' );
 	aaa = aaa.replace(']', ')' );
-	let answer = eval(aaa.toString());
+	let answer = eval(aaa.replace(/[(]\d+|[)]/ig,"").toString());
       
   if(text1 != null){
 	  finalStr= text0 + '：' + text1 + '\n' + equation + ' = ' + answer;
