1d043f793ed3520ea738e4c1cdee0b6324196724
zeteticl <cougar.l@hotmail.com>
2018-02-01 01:19:05 +0800

    v0.9

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var coc = require('./roll/coc.js');
 var rollbase = require('./roll/rollbase.js');
 var advroll = require('./roll/advroll.js');
 var nc = require('./roll/nc.js');
+var wod = require('./roll/wod.js');
+var funny = require('./roll/funny.js');
 var options = {
   host: 'api.line.me',
   port: 443,
@@ -99,13 +101,13 @@ function parseInput(rplyToken, inputStr) {
 		let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
                        
         //鴨霸獸指令開始於此
-        if (trigger.match(/鴨霸獸|巴獸/) != null) return randomReply() ;        
-        if (trigger.match(/運氣|運勢/) != null) return randomLuck(mainMsg) ; //占卜運氣        
+        if (trigger.match(/鴨霸獸|巴獸/) != null) return funny.randomReply() ;        
+        if (trigger.match(/運氣|運勢/) != null) return funny.randomLuck(mainMsg) ; //占卜運氣        
         
 		//FLAG指令開始於此
-        if (trigger.match(/立flag|死亡flag/) != null) return BStyleFlagSCRIPTS() ;        
+        if (trigger.match(/立flag|死亡flag/) != null) return funny.BStyleFlagSCRIPTS() ;        
        
-        if (trigger.match(/^coc7角色背景$/)!= null ) return PcBG();
+        if (trigger.match(/^coc7角色背景$/)!= null ) return coc.PcBG();
 		
 		//nc指令開始於此 來自Rainsting/TarotLineBot 
 		if (trigger.match(/^[1-4]n[c|a][+|-][1-99]$|^[1-4]n[c|a]$/)!= null ) return nc.nechronica(trigger,mainMsg[1]);
@@ -123,7 +125,7 @@ function parseInput(rplyToken, inputStr) {
  	* Fisher–Yates shuffle
  	  SortIt 指令開始於此
  	*/
- 	if (trigger.match(/排序/)!= null && mainMsg.length >= 3) return SortIt(inputStr,mainMsg);
+ 	if (trigger.match(/排序/)!= null && mainMsg.length >= 3) return funny.SortIt(inputStr,mainMsg);
  	if (trigger.match(/^d66$/)!= null ) return advroll.d66(mainMsg[1]);
 	if (trigger.match(/^d66s$/)!= null ) return advroll.d66s(mainMsg[1]);
 	if (trigger.match(/^ccb$|^cc$|^ccn[1-2]$|^cc[1-2]$/)!= null && mainMsg[1]<=1000 )
@@ -142,16 +144,16 @@ function parseInput(rplyToken, inputStr) {
 	}
 	
 	//wod 指令開始於此
-	if (trigger.match(/^(\d+)(wd|wod)(\d|)((\+|-)(\d+)|)$/i)!= null)return wod(trigger,mainMsg[1]);
+	if (trigger.match(/^(\d+)(wd|wod)(\d|)((\+|-)(\d+)|)$/i)!= null)return wod.wod(trigger,mainMsg[1]);
 	
 	//choice 指令開始於此
-	if (trigger.match(/choice|隨機|選項|選1/)!= null && mainMsg.length >= 3) return choice(inputStr,mainMsg);
+	if (trigger.match(/choice|隨機|選項|選1/)!= null && mainMsg.length >= 3) return funny.choice(inputStr,mainMsg);
 	//tarot 指令
 	if (trigger.match(/tarot|塔羅牌|塔羅/) != null) {
-			if (trigger.match(/每日|daily/)!= null) return NomalDrawTarot(mainMsg[1], mainMsg[2]);
-			if (trigger.match(/時間|time/)!= null) 	return MultiDrawTarot(mainMsg[1], mainMsg[2], 1);
-			if (trigger.match(/大十字|cross/)!= null) return MultiDrawTarot(mainMsg[1], mainMsg[2], 2);
-			return MultiDrawTarot(mainMsg[1], mainMsg[2], 3); //預設抽 79 張
+			if (trigger.match(/每日|daily/)!= null) return funny.NomalDrawTarot(mainMsg[1], mainMsg[2]);
+			if (trigger.match(/時間|time/)!= null) 	return funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 1);
+			if (trigger.match(/大十字|cross/)!= null) return funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 2);
+			return funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 3); //預設抽 79 張
 		}
 
 		/*tarot 指令
@@ -173,434 +175,6 @@ function parseInput(rplyToken, inputStr) {
 ////////////////////////////////////////
 //////////////// 骰組開始
 ////////////////////////////////////////      
-
-
-////////////////////////////////////////
-//////////////// WOD黑暗世界
-////////////////////////////////////////
-
-function wod(triggermsg ,text) {
-	var returnStr = triggermsg+' [';
-	var varcou = 0;
-	var varsu = 0;
-	var match = /^(\d+)(wd|wod)(\d|)((\+|-)(\d+)|)$/i.exec(triggermsg);   //判斷式  [0]3wd8+10,[1]3,[2]wd,[3]8,[4]+10,[5]+,[6]10  
-	if (match[3]=="") { match[3] =10 }
-	if (match[3]<=2) { return '加骰最少比2高'; }
-			
-for (var i = 0; i < Number(match[1]); i++)	
-	{
-             varcou =  Math.floor(Math.random() * 10) + 1;
-             returnStr += varcou +', ';
-             
-		
-             if (varcou >=match[3]) { i--}
-             if (varcou >=8) 
-	     {
-		     varsu++;
-	     }
-
-	}
-
-	    if(match[5]=='+'){
-    
-    for (var i = 0; i < Number(match[6]); i++)	{
-	    varsu++;
-    }
-    }
-    if(match[5]=='-'){
-    
-    for (var i = 0; i < Number(match[6]); i++)	{
-	    varsu--;
-    }
-    }
-	
-    returnStr = returnStr.replace(/[,][ ]$/,'] → '+varsu+'成功');
-	if (text != null){
-	returnStr += ' ; ' + text;
-	}
-	return returnStr;
-}
-////////////////////////////////////////
-//////////////// 占卜&其他
-////////////////////////////////////////
-
-
-function BStyleFlagSCRIPTS() {
-          let rplyArr = ['\
-「打完這仗我就回老家結婚（この戦いが終わったら、故郷に帰って結婚するんだ）」', '\
-「打完這一仗後我請你喝酒」', '\
-「你、你要錢嗎！要什麼我都能給你！/我可以給你更多的錢！」', '\
-「做完這次任務，我就要結婚了。」', '\
-「幹完這一票我就金盆洗手了。」', '\
-「好想再XXX啊……」', '\
-「已經沒什麼好害怕的了（もう何も恐くない）」', '\
-「我一定會回來的（必ず帰る！）」', '\
-「差不多該走了」', '\
-「我只是希望你永遠不要忘記我。」', '\
-「我只是希望能永遠和你在一起。」', '\
-「啊啊…為什麼會在這種時候、想起了那些無聊的事呢？」', '\
-「能遇見你真是太好了。」', '\
-「我終於…為你們報仇了！」', '\
-「等到一切結束後，我有些話想跟妳說！」', '\
-「這段時間我過的很開心啊。」', '\
-把自己的寶物借給其他人，然後說「待一切結束後記得還給我。」', '\
-「真希望這份幸福可以永遠持續下去。」', '\
-「我們三個人要永永遠遠在一起！」', '\
-「這是我女兒的照片，很可愛吧？」', '\
-「請告訴他/她，我永遠愛他/她」', '\
-「聽好，在我回來之前絕不要亂走動哦（いいか、俺が帰ってくるまでここを動くんじゃないぞ）」', '\
-「要像一個乖孩子一樣等著我回來」', '\
-「我去去就來（先に行って、すぐ戻るから）」', '\
-「快逃！(逃げろう！/早く逃げろう！)」', '\
-「對方只有一個人，大家一起上啊」', '\
-「我就不信，這麼多人還殺不了他一個！」', '\
-「幹，幹掉了嗎？（やったのか？）」', '\
-「身體好輕」', '\
-「可惡！你給我看著！（逃跑）」', '\
-「躲在這裡就應該不會被發現了吧。」', '\
-「我不會讓任何人死的。」', '\
-「可惡！原來是這麼回事！」', '\
-「跑這麼遠應該就行了。」', '\
-「我已經甚麼都不怕了（もう何も恐くない）」', '\
-「這XXX是什麼，怎麼之前沒見過（なんだこのXXX、見たことないな）」', '\
-「什麽聲音……？就去看一下吧（:「何の音だ？ちょっと見てくる」', '\
-「是我的錯覺嗎？/果然是錯覺/錯覺吧/可能是我（看/聽）錯了」', '\
-「二十年後又是一條好漢！」', '\
-「大人/將軍武運昌隆」', '\
-「這次工作的報酬是以前無法比較的（:「今度の仕事でまとまったカネが入るんだ」', '\）」', '\
-「我才不要和罪犯呆在一起，我回自己的房間去了！（この中に殺人者がいるかもしれないのに、一緒に居られるか!俺は自分の部屋に戻るぞ!）」', '\
-「其實我知道事情的真相…（各種廢話）…犯人就是……」', '\
-「我已經天下無敵了~~」', '\
-「大人！這邊就交給小的吧，請快離開這邊吧」', '\
-「XX，這就是我們流派的最終奧義。這一招我只會演示一次，你看好了！」', '\
-「誰敢殺我？」', '\
-「從來沒有人能越過我的劍圍。」', '\
-「就算殺死也沒問題吧？」', '\
-「看我塔下強殺！」', '\
-「騙人的吧，我們不是朋友嗎？」', '\
-「我老爸是....你有種就....」', '\
-「我可以好好利用這件事」'];
-          return rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
-        }
-	
-        function randomReply() {
-          let rplyArr = ['\
-你們死定了呃呃呃不要糾結這些……所以是在糾結哪些？', '\
-在澳洲，每過一分鐘就有一隻鴨嘴獸被拔嘴。 \n我到底在共三小。', '\
-嗚噁噁噁噁噁噁，不要隨便叫我。', '\
-幹，你這學不會的豬！', '\
-嘎嘎嘎。', '\
-wwwwwwwwwwwwwwwww', '\
-為什麼你們每天都可以一直玩；玩就算了還玩我。', '\
-好棒，整點了！咦？不是嗎？', '\
-不要打擾我挖坑！', '好棒，誤點了！', '\
-在南半球，一隻鴨嘴獸拍打他的鰭，他的嘴就會掉下來。 \n我到底在共三小。', '\
-什麼東西你共三小。', '\
-哈哈哈哈哈哈哈哈！', '\
-一直叫，你4不4想拔嘴人家？', '\
-一直叫，你想被淨灘嗎？', '\
-幫主你也敢嘴？', '\
-拔嘴的話，我的嘴巴會長出觸手，然後開花成四個花瓣哦 (´×`)', '\
-看看我！！我體內的怪物已經這麼大了！！', '\
-傳說中，凡是拔嘴過鴨嘴獸的人，有高機率在100年內死去。 \n我到底在共三小。', '\
-人類每花60秒拔嘴，就減少一分鐘的壽命。 \n我到底在共三小。', '\
-嘴被拔，就會掉。', '\
-你在大聲什麼啦！！！！', '\
-公道價，八萬一（伸手）。', '\
-你的嘴裡有異音（指）', '\
-幫主說，有人打你的左臉，你就要用肉食性猛擊咬斷他的小腿。'];
-          return rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
-        }
-		
-       function randomLuck(TEXT) {
-           let rplyArr = ['超吉','超級上吉','大吉','吉','中吉','小吉','吉','小吉','吉','吉','中吉','吉','中吉','吉','中吉','小吉','末吉','吉','中吉','小吉','末吉','中吉','小吉','小吉','吉','小吉','末吉','中吉','小吉','凶','小凶','沒凶','大凶','很凶'];
-           return TEXT[0] + ' ： ' + rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
-        }
-		
-		
-////////////////////////////////////////
-//////////////// Funny
-////////////////////////////////////////
-/* 猜拳功能 */
-	function RockPaperScissors(HandToCal, text) {
-	let returnStr = '';
-	if (HandToCal.match(/石頭|布|剪刀|1|2|3/) != null) {
-		let aHand = ['石頭','布','剪刀'];
-           HandToCal = aHand[Math.floor((Math.random() * (aHand.length)) + 0)];
-	}
-	var hand = FunnyDice(3); // 0:石頭 1:布 2:剪刀
-
-	switch (hand) {
-		case 0: //石頭
-			returnStr = '我出石頭！\n';
-
-			if (HandToCal.match(/剪刀|1/) != null) returnStr += '哼哼你輸惹';
-			else if (HandToCal.match(/石頭|2/) != null) returnStr += '看來我們不相上下阿';
-			else if (HandToCal.match(/布|3/) != null) returnStr += '你好像有點強！';
-			else returnStr += '欸不對喔你亂出！';
-
-			break;
-
-		case 1: //布
-			returnStr = '我出布！\n';
-
-			if (HandToCal.match(/剪刀|1/) != null) returnStr += '讓你一次而已啦！';
-			else if (HandToCal.match(/布|2/) != null) returnStr += '原來平手...沒什麼嘛！';
-			else if (HandToCal.match(/石頭|3/) != null) returnStr += '哈哈你看看你！';
-			else returnStr += '別亂出阿會壞掉的';
-
-			break;
-
-		case 2: //剪刀
-			returnStr = '我出剪刀！\n';
-
-			if (HandToCal.match(/剪刀|1/) != null) returnStr += '平手 (  艸)';
-			else if (HandToCal.match(/布|2/) != null) returnStr += '贏了 (｀・ω・´)b';
-			else if (HandToCal.match(/石頭|3/) != null) returnStr += '輸惹 ゜。。゜(ノД‵)ノ・゜';
-			else returnStr += '亂出打你喔 (｀・ω・´)凸';
-
-			break;
-
-		default:
-			returnStr = '我出的是...欸不對你沒出喔！\n';
-			break;
-	}
-
-	return returnStr;
-}
-
-
-
-////////////////////////////////////////
-//////////////// Tarot塔羅牌
-////////////////////////////////////////
-function MultiDrawTarot(CardToCal, text, type) {
-	let returnStr = '';
-	var tmpcard = 0;
-	var cards = [];
-	var revs = [];
-	var i = 0;
-
-	if (type == 1) //時間之流
-	{
-		cards[0] = FunnyDice(79); //先抽第0張
-		revs[0] = FunnyDice(2);
-
-		for (i = 1; i < 3; i++) {
-			for (;;) {
-				tmpcard = FunnyDice(79);
-				if (cards.indexOf(tmpcard) === -1) //沒有重複，就這張了
-				{
-					cards.push(tmpcard);
-					revs[i] = FunnyDice(2);
-					break;
-				}
-			}
-		}
-
-		if (text != null)
-			returnStr += text + ': \n';
-
-		for (i = 0; i < 3; i++) {
-			if (i == 0) returnStr += '過去: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-			if (i == 1) returnStr += '現在: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-			if (i == 2) returnStr += '未來: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]);
-		}
-
-	} else if (type == 2) //塞爾特大十字
-	{
-		cards[0] = FunnyDice(79); //先抽第0張
-		revs[0] = FunnyDice(2);
-
-		for (i = 1; i < 10; i++) {
-			for (;;) {
-				tmpcard = FunnyDice(79);
-				if (cards.indexOf(tmpcard) === -1) //沒有重複，就這張了
-				{
-					cards.push(tmpcard);
-					revs[i] = FunnyDice(2);
-					break;
-				}
-			}
-		}
-
-		if (text != null)
-			returnStr += text + ': \n';
-
-		for (i = 0; i < 10; i++) {
-			if (i == 0) returnStr += '現況: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-			if (i == 1) {
-				if (revs[i] == 0) //正位
-					returnStr += '助力: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-				else
-					returnStr += '阻力: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-			}
-			if (i == 2) returnStr += '目標: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-			if (i == 3) returnStr += '基礎: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-			if (i == 4) returnStr += '過去: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-			if (i == 5) returnStr += '未來: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-			if (i == 6) returnStr += '自我: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-			if (i == 7) returnStr += '環境: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-			if (i == 8) {
-				if (revs[i] == 0) //正位
-					returnStr += '希望: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-				else
-					returnStr += '恐懼: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
-			}
-			if (i == 9) returnStr += '結論: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]);
-
-		}
-
-	} else {
-
-		if (text == null)
-			returnStr = tarotCardReply(FunnyDice(79)) + ' ' + tarotRevReply(FunnyDice(2));
-		else
-			returnStr = tarotCardReply(FunnyDice(79)) + ' ' + tarotRevReply(FunnyDice(2)) + ' ; ' + text;
-	}
-
-
-	return returnStr;
-}
-
-function NomalDrawTarot(CardToCal, text) {
-	let returnStr = '';
-
-	if (text == null)
-		returnStr = tarotCardReply(FunnyDice(22)) + ' ' + tarotRevReply(FunnyDice(2));
-	else
-		returnStr = tarotCardReply(FunnyDice(22)) + ' ' + tarotRevReply(FunnyDice(2)) + ' ; ' + text;
-	return returnStr;
-}
-
-
- function SortIt(input,mainMsg) {   
- 
- 	let a = input.replace(mainMsg[0], '').match(/\S+/ig);
-     for (var i = a.length-1; i >=0; i--) {
- 
-         var randomIndex = Math.floor(Math.random()*(i+1));
-         var itemAtIndex = a[randomIndex];
-         a[randomIndex] = a[i];
-         a[i] = itemAtIndex;
-     }
-     	return mainMsg[0] + ' → ['+ a + ']' ;
- }
-
-function tarotRevReply(count) {
-	let returnStr = '';
-
-	if (count == 0) returnStr = '＋';
-	if (count == 1) returnStr = '－';
-
-	return returnStr;
-}
-
-function choice(input,str) {
-	let a = input.replace(str[0], '').match(/\S+/ig);
-	return str[0] + '['+ a + '] → ' + a[rollbase.Dice(a.length)-1];
-}
-
-function tarotCardReply(count) {
-	let returnStr = '';
-	// returnStr = count + '愚者';
-	if (count == 0) returnStr = '愚者';
-	if (count == 1) returnStr = '魔術師';
-	if (count == 2) returnStr = '女祭司';
-	if (count == 3) returnStr = '女皇';
-	if (count == 4) returnStr = '皇帝';
-	if (count == 5) returnStr = '教皇';
-	if (count == 6) returnStr = '戀人';
-	if (count == 7) returnStr = '戰車';
-	if (count == 8) returnStr = '力量';
-	if (count == 9) returnStr = '隱者';
-	if (count == 10) returnStr = '命運之輪';
-	if (count == 11) returnStr = '正義';
-	if (count == 12) returnStr = '吊人';
-	if (count == 13) returnStr = '死神';
-	if (count == 14) returnStr = '節制';
-	if (count == 15) returnStr = '惡魔';
-	if (count == 16) returnStr = '高塔';
-	if (count == 17) returnStr = '星星';
-	if (count == 18) returnStr = '月亮';
-	if (count == 19) returnStr = '太陽';
-	if (count == 20) returnStr = '審判';
-	if (count == 21) returnStr = '世界';
-	if (count == 22) returnStr = '權杖一';
-	if (count == 23) returnStr = '權杖二';
-	if (count == 24) returnStr = '權杖三';
-	if (count == 25) returnStr = '權杖四';
-	if (count == 26) returnStr = '權杖五';
-	if (count == 27) returnStr = '權杖六';
-	if (count == 28) returnStr = '權杖七';
-	if (count == 29) returnStr = '權杖八';
-	if (count == 30) returnStr = '權杖九';
-	if (count == 31) returnStr = '權杖十';
-	if (count == 32) returnStr = '權杖侍者';
-	if (count == 33) returnStr = '權杖騎士';
-	if (count == 34) returnStr = '權杖皇后';
-	if (count == 35) returnStr = '權杖國王';
-	if (count == 36) returnStr = '聖杯一';
-	if (count == 37) returnStr = '聖杯二';
-	if (count == 38) returnStr = '聖杯三';
-	if (count == 39) returnStr = '聖杯四';
-	if (count == 40) returnStr = '聖杯五';
-	if (count == 41) returnStr = '聖杯六';
-	if (count == 42) returnStr = '聖杯七';
-	if (count == 43) returnStr = '聖杯八';
-	if (count == 44) returnStr = '聖杯九';
-	if (count == 45) returnStr = '聖杯十';
-	if (count == 46) returnStr = '聖杯侍者';
-	if (count == 47) returnStr = '聖杯騎士';
-	if (count == 48) returnStr = '聖杯皇后';
-	if (count == 49) returnStr = '聖杯國王';
-	if (count == 50) returnStr = '寶劍一';
-	if (count == 51) returnStr = '寶劍二';
-	if (count == 52) returnStr = '寶劍三';
-	if (count == 53) returnStr = '寶劍四';
-	if (count == 54) returnStr = '寶劍五';
-	if (count == 55) returnStr = '寶劍六';
-	if (count == 56) returnStr = '寶劍七';
-	if (count == 57) returnStr = '寶劍八';
-	if (count == 58) returnStr = '寶劍九';
-	if (count == 59) returnStr = '寶劍十';
-	if (count == 60) returnStr = '寶劍侍者';
-	if (count == 61) returnStr = '寶劍騎士';
-	if (count == 62) returnStr = '寶劍皇后';
-	if (count == 63) returnStr = '寶劍國王';
-	if (count == 64) returnStr = '錢幣一';
-	if (count == 65) returnStr = '錢幣二';
-	if (count == 66) returnStr = '錢幣三';
-	if (count == 67) returnStr = '錢幣四';
-	if (count == 68) returnStr = '錢幣五';
-	if (count == 69) returnStr = '錢幣六';
-	if (count == 70) returnStr = '錢幣七';
-	if (count == 71) returnStr = '錢幣八';
-	if (count == 72) returnStr = '錢幣九';
-	if (count == 73) returnStr = '錢幣十';
-	if (count == 74) returnStr = '錢幣侍者';
-	if (count == 75) returnStr = '錢幣騎士';
-	if (count == 76) returnStr = '錢幣皇后';
-	if (count == 77) returnStr = '錢幣國王';
-	if (count == 78) returnStr = '空白牌';
-
-	return returnStr;
-
-}
-  //隨機產生角色背景
-  function PcBG(){
-    let PersonalDescriptionArr = ['結實的', '英俊的', '粗鄙的', '機靈的', '迷人的', '娃娃臉的', '聰明的', '蓬頭垢面的', '愚鈍的', '骯髒的', '耀眼的', '有書卷氣的','青春洋溢的','感覺疲憊的','豐滿的','粗壯的','毛髮茂盛的','苗條的','優雅的','邋遢的','敦實的','蒼白的','陰沉的','平庸的','臉色紅潤的','皮膚黝黑色','滿臉皺紋的','古板的','有狐臭的','狡猾的','健壯的','嬌俏的','筋肉發達的','魁梧的','遲鈍的', '虛弱的'];
-    let IdeologyBeliefsArr = ['虔誠信仰著某個神祈','覺得人類不需要依靠宗教也可以好好生活','覺得科學可以解釋所有事，並對某種科學領域有獨特的興趣','相信因果循環與命運','是一個政黨、社群或秘密結社的成員','覺得這個社會已經病了，而其中某些病灶需要被剷除','是神秘學的信徒','是積極參與政治的人，有特定的政治立場','覺得金錢至上，且為了金錢不擇手段','是一個激進主義分子，活躍於社會運動'];
-    let SignificantPeopleArr = ['他的父母', '他的祖父母', '他的兄弟姐妹', '他的孩子', '他的另一半', '那位曾經教導調查員最擅長的技能（點數最高的職業技能）的人','他的兒時好友', '他心目中的偶像或是英雄', '在遊戲中的另一位調查員', '一個由KP指定的NPC'];
-    let SignificantPeopleWhyArr = ['調查員在某種程度上受了他的幫助，欠了人情','調查員從他那裡學到了些什麼重要的東西','他給了調查員生活的意義','調查員曾經傷害過他，尋求他的原諒','和他曾有過無可磨滅的經驗與回憶','調查員想要對他證明自己','調查員崇拜著他','調查員對他有著某些使調查員後悔的過往','調查員試圖證明自己和他不同，比他更出色','他讓調查員的人生變得亂七八糟，因此調查員試圖復仇'];
-    let MeaningfulLocationsArr = ['過去就讀的學校','他的故鄉','與他的初戀之人相遇之處','某個可以安靜沉思的地方','某個類似酒吧或是熟人的家那樣的社交場所','與他的信念息息相關的地方','埋葬著某個對調查員別具意義的人的墓地','他從小長大的那個家','他生命中最快樂時的所在','他的工作場所'];
-    let TreasuredPossessionsArr = ['一個與他最擅長的技能（點數最高的職業技能）相關的物品','一件他的在工作上需要用到的必需品','一個從他童年時就保存至今的寶物','一樣由調查員最重要的人給予他的物品','一件調查員珍視的蒐藏品','一件調查員無意間發現，但不知道到底是什麼的東西，調查員正努力尋找答案','某種體育用品','一把特別的武器','他的寵物'];
-    let TraitsArr = ['慷慨大方的人','對動物很友善的人','善於夢想的人','享樂主義者','甘冒風險的賭徒或冒險者', '善於料理的人', '萬人迷','忠心耿耿的人','有好名聲的人','充滿野心的人'];
-    
-    return '背景描述生成器（僅供娛樂用，不具實際參考價值）\n==\n調查員是一個' + PersonalDescriptionArr[Math.floor((Math.random() * (PersonalDescriptionArr.length)) + 0)] + '人。\n【信念】：說到這個人，他' + IdeologyBeliefsArr[Math.floor((Math.random() * (IdeologyBeliefsArr.length)) + 0)] + '。\n【重要之人】：對他來說，最重要的人是' + SignificantPeopleArr[Math.floor((Math.random() * (SignificantPeopleArr.length)) + 0)] + '，這個人對他來說之所以重要，是因為' + SignificantPeopleWhyArr[Math.floor((Math.random() * (SignificantPeopleWhyArr.length)) + 0)] + '。\n【意義非凡之地】：對他而言，最重要的地點是' + MeaningfulLocationsArr[Math.floor((Math.random() * (MeaningfulLocationsArr.length)) + 0)] + '。\n【寶貴之物】：他最寶貴的東西就是'+ TreasuredPossessionsArr[Math.floor((Math.random() * (TreasuredPossessionsArr.length)) + 0)] + '。\n【特徵】：總括來說，調查員是一個' + TraitsArr[Math.floor((Math.random() * (TraitsArr.length)) + 0)] + '。';
-    
-  }
-
 
 		function Help() {
 			return randomReply() + '\n' + '\
--- roll/coc.js
+++ roll/coc.js
@@ -261,6 +261,21 @@ function build6char(){
 	ReStr = ReStr + '\n調查員的最小起始年齡等於EDU+6，每比起始年齡年老十年，\n調查員增加一點EDU並且加20點職業技能點數。\n當超過40歲後，每老十年，\n從STR,CON,DEX,APP中選擇一個減少一點。';
     return ReStr;
   } 
+  
+    //隨機產生角色背景
+  function PcBG(){
+    let PersonalDescriptionArr = ['結實的', '英俊的', '粗鄙的', '機靈的', '迷人的', '娃娃臉的', '聰明的', '蓬頭垢面的', '愚鈍的', '骯髒的', '耀眼的', '有書卷氣的','青春洋溢的','感覺疲憊的','豐滿的','粗壯的','毛髮茂盛的','苗條的','優雅的','邋遢的','敦實的','蒼白的','陰沉的','平庸的','臉色紅潤的','皮膚黝黑色','滿臉皺紋的','古板的','有狐臭的','狡猾的','健壯的','嬌俏的','筋肉發達的','魁梧的','遲鈍的', '虛弱的'];
+    let IdeologyBeliefsArr = ['虔誠信仰著某個神祈','覺得人類不需要依靠宗教也可以好好生活','覺得科學可以解釋所有事，並對某種科學領域有獨特的興趣','相信因果循環與命運','是一個政黨、社群或秘密結社的成員','覺得這個社會已經病了，而其中某些病灶需要被剷除','是神秘學的信徒','是積極參與政治的人，有特定的政治立場','覺得金錢至上，且為了金錢不擇手段','是一個激進主義分子，活躍於社會運動'];
+    let SignificantPeopleArr = ['他的父母', '他的祖父母', '他的兄弟姐妹', '他的孩子', '他的另一半', '那位曾經教導調查員最擅長的技能（點數最高的職業技能）的人','他的兒時好友', '他心目中的偶像或是英雄', '在遊戲中的另一位調查員', '一個由KP指定的NPC'];
+    let SignificantPeopleWhyArr = ['調查員在某種程度上受了他的幫助，欠了人情','調查員從他那裡學到了些什麼重要的東西','他給了調查員生活的意義','調查員曾經傷害過他，尋求他的原諒','和他曾有過無可磨滅的經驗與回憶','調查員想要對他證明自己','調查員崇拜著他','調查員對他有著某些使調查員後悔的過往','調查員試圖證明自己和他不同，比他更出色','他讓調查員的人生變得亂七八糟，因此調查員試圖復仇'];
+    let MeaningfulLocationsArr = ['過去就讀的學校','他的故鄉','與他的初戀之人相遇之處','某個可以安靜沉思的地方','某個類似酒吧或是熟人的家那樣的社交場所','與他的信念息息相關的地方','埋葬著某個對調查員別具意義的人的墓地','他從小長大的那個家','他生命中最快樂時的所在','他的工作場所'];
+    let TreasuredPossessionsArr = ['一個與他最擅長的技能（點數最高的職業技能）相關的物品','一件他的在工作上需要用到的必需品','一個從他童年時就保存至今的寶物','一樣由調查員最重要的人給予他的物品','一件調查員珍視的蒐藏品','一件調查員無意間發現，但不知道到底是什麼的東西，調查員正努力尋找答案','某種體育用品','一把特別的武器','他的寵物'];
+    let TraitsArr = ['慷慨大方的人','對動物很友善的人','善於夢想的人','享樂主義者','甘冒風險的賭徒或冒險者', '善於料理的人', '萬人迷','忠心耿耿的人','有好名聲的人','充滿野心的人'];
+    
+    return '背景描述生成器（僅供娛樂用，不具實際參考價值）\n==\n調查員是一個' + PersonalDescriptionArr[Math.floor((Math.random() * (PersonalDescriptionArr.length)) + 0)] + '人。\n【信念】：說到這個人，他' + IdeologyBeliefsArr[Math.floor((Math.random() * (IdeologyBeliefsArr.length)) + 0)] + '。\n【重要之人】：對他來說，最重要的人是' + SignificantPeopleArr[Math.floor((Math.random() * (SignificantPeopleArr.length)) + 0)] + '，這個人對他來說之所以重要，是因為' + SignificantPeopleWhyArr[Math.floor((Math.random() * (SignificantPeopleWhyArr.length)) + 0)] + '。\n【意義非凡之地】：對他而言，最重要的地點是' + MeaningfulLocationsArr[Math.floor((Math.random() * (MeaningfulLocationsArr.length)) + 0)] + '。\n【寶貴之物】：他最寶貴的東西就是'+ TreasuredPossessionsArr[Math.floor((Math.random() * (TreasuredPossessionsArr.length)) + 0)] + '。\n【特徵】：總括來說，調查員是一個' + TraitsArr[Math.floor((Math.random() * (TraitsArr.length)) + 0)] + '。';
+    
+  }
+
 
 
 
@@ -271,5 +286,6 @@ module.exports = {
 	coc7bp: coc7bp,
 	ArrMax: ArrMax,
 	build7char: build7char,
-	build6char: build6char
+	build6char: build6char,
+	PcBG:PcBG
 };
