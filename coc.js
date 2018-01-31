a637b2a99c5205f035de0f4c96ca7a6a68eb37e0
zeteticl <cougar.l@hotmail.com>
2018-01-31 16:18:25 +0800

    test

---
+++ coc.js
@@ -0,0 +1,25 @@
+	
+	
+	////////////////////////////////////////
+//////////////// COC6
+////////////////////////////////////////      
+    
+
+function coc6(chack,text){
+          let temp = Dice(100);
+          if (text == null ) {
+            if (temp == 100) return 'ccb<=' + chack  + ' ' + temp + ' → 啊！大失敗！';
+            if (temp <= chack) return 'ccb<=' + chack + ' '  + temp + ' → 成功';
+            else return 'ccb<=' + chack  + ' ' + temp + ' → 失敗' ;
+          }
+          else
+    {
+            if (temp == 100) return 'ccb<=' + chack + ' ' + temp + ' → 啊！大失敗！；' + text;
+            if (temp <= chack) return 'ccb<=' + chack +  ' ' + temp + ' → 成功；' + text;
+            else return 'ccb<=' + chack  + ' ' +  temp + ' → 失敗；' + text;
+    }
+}        
+
+
+
+module.exports.coc26 = coc6;  
