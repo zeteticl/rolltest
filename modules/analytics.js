794c3c2becb8f29c3f207666b1a4f1f9fb18f2ba
zeteticl <cougar.l@hotmail.com>
2018-02-03 12:44:57 +0800

    test

--- modules/analytics.js
+++ modules/analytics.js
@@ -1,11 +1,12 @@
 // Load `*.js` under current directory as properties
 //  i.e., `User.js` will become `exports['User']` or `exports.User`
-require('fs').readdirSync(__dirname + '../roll/').forEach(function(file) {
-  if (file.match(/\.js$/) !== null && file !== 'index.js') {
-    var name = file.replace('.js', '');
-    exports[name] = require('../roll/' + file);
-  }
-});
+require('fs').readdirSync('./roll/').forEach(function(file) {
+	if (file.match(/\.js$/) !== null && file !== 'index.js') {
+	  var name = file.replace('.js', '');
+	  exports[name] = require('../roll/' + file);
+	}
+  });
+
 //用來呼叫骰組,新增骰組檔案的話,要寫到上面
 function parseInput(rplyToken, inputStr) {
 	//console.log('InputStr: ' + inputStr);
